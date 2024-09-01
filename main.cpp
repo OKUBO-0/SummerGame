1cc90df4e337a0274b16dd759bfc191dcecec432
OKUBO-0 <k023g0020@g.neec.ac.jp>
2024-09-01 18:37:48 +0900

    夏の自作ゲーム

--- main.cpp
+++ main.cpp
@@ -86,7 +86,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"LE2C_06_オオクボ_タク_AL3");
+	win->CreateGameWindow(L"Slide_UP!");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
